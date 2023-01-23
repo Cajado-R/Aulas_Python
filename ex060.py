ec89e58821bd370684f978a71d88812fb6c8ed38
Cláudio Cajado <101744023+Cajado-R@users.noreply.github.com>
2023-01-23 16:47:32 -0300

    initial commit

--- ex060.py
+++ ex060.py
@@ -3,13 +3,20 @@ ls = int
 for c in range (num):
     ls = (num-1) * num
     print(num)"""
+import math
+print(30*'-')
+print('VENHA CALCULAR O FATORIAL')
+print(30*'-')
+soma = 0
 num = int(input('Digite um número para recebero fatorial: '))
-fa = num
-to = 1
-print('{}'.format(num), end='')
-while fa > 0:
-   print('{}'.format(fa), end='')
-   print(' X 'if fa > 1 else ' = ',end='')
-   to *= fa
-   fa -= 1
-print('{}'.format(to))
+fato = num
+rial = 1
+while fato > 0:
+   #print que adiciona o número do fatorial primeiro
+   print(f'{fato}', end='')
+   #print que imprime X caso o número do fatorial seja maior que 1 senão imprime igual
+   print(' X ' if fato > 1 else ' = ',end='')
+   rial *= fato
+   fato -= 1
+   resultado = rial
+print(f'{resultado}')
--- ex063.py
+++ ex063.py
@@ -1,17 +1,14 @@
 print('Sequência de Fibonacci')
-n = int(input('Digite quantos numeros da sequencia gostaria de ver? ; '))
-c = 0
-f = 0
-if c == 0:
-    print(f)
-    c = 1
-    f2 = 1
-    print(f2)
-    while c < (n - 1):
-        x = f + f2
-        print(x)
-        c += 1
-        f = f2
-        f2 = x
-print('')
-print(f'Sequência finalizada com {n} termos.')
+numeros = int(input('Digite quantos numeros da sequencia gostaria de ver? :'))
+comeco = 0
+fibonacci = 0
+if comeco == 1:
+    comeco = 1
+    fibonacci2 = 1
+    while comeco < (numeros - 1):
+        x = fibonacci + fibonacci2
+        print(x, end='>>>')
+        comeco += 1
+        fibonacci = fibonacci2
+        fibonacci2 = x
+print(f'\nSua sequência foi finalizada!')
